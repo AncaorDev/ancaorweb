9f82b91456dbcff2dd9b18e76f0b96b68ae5e09f
RypCode_AC <ancaors1@gmail.com>
2017-09-16 13:49:11 -0500

    actualización proxima a v0.0.1

--- app/server.php
+++ app/server.php
@@ -5,22 +5,17 @@
 class Server
 {
 	
-	function __construct()
-	{
-		
-	}
-	public static function Time($value='')
+	public static function Time()
 	{
 		$tiempo_inicio = microtime(true);
 		DEFINE('TIME_INICIO', $tiempo_inicio);		
 	}
-
-	public static function endTime($value='')
+	public static function endTime()
 	{
 		$tiempo_fin = microtime(true);
 		$total = bcsub($tiempo_fin, TIME_INICIO, 3);
-		echo "<div style='background: black;text-align:center;color:white;'>
-			<p style='margin:0;'>Esta página fue generada en {$total} segundos. </p>
-			</div>";
+		$html = "<div style='background: black;text-align:center;color:white;'><p style='margin:0;'>";
+		$html .= "Esta página fue generada en {$total} segundos. </p></div>";
+		print($html);
 	}
 }
